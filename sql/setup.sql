69feba19a511d78940db19fe9b37f76ef4677905
dl-watson <dlarkinwatson@gmail.com>
2020-12-16 13:55:03 -0800

    modifying max url size

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS books;
 CREATE TABLE books (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     book_title TEXT NOT NULL,
-    cover_img_url VARCHAR(512) NOT NULL,
+    cover_img_url VARCHAR(2083) NOT NULL,
     book_rating INT NOT NULL CHECK( book_rating >= 0 AND book_rating <= 5),
     title_in_stock BOOLEAN NOT NULL
 );
